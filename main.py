e3744cfb19f17f2c85acc5a048366f4083593146
viswanathanl <viswanathanl@chola.murugappa.com>
2025-02-21 12:27:04 +0530

    update

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 from fastapi import FastAPI
 from fastapi import APIRouter,Request,HTTPException
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse,HTMLResponse
 templates = Jinja2Templates(directory="")
 from fastapi.responses import JSONResponse
+import pymysql
+from pymysql.cursors import DictCursor
+from fastapi.staticfiles import StaticFiles
 
 
+connection=pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='test_db',
+        cursorclass=DictCursor
+    )
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get('/home')
 def homepage(request:Request):
-    return templates.TemplateResponse('home.html', context={'request': request})
+
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM projects.projects"
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+
+
+    return templates.TemplateResponse('architecture.html', context={'request': request,'data':data})
+
+
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse("logo.png")
 
-@app.middleware("http")
-async def enforce_https(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains; preload"
-    response.headers["X-Frame-Options"] = "DENY"
-    response.headers['X-Content-Type-Options']= "nosniff"
-    response.headers["Content-Security-Policy"] = "default-src 'self'"
-    response.headers["X-XSS-Protection"] = "1; mode=block"
-    return response
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
